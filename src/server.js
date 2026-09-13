04bbca541fdcbfe0e0ff04aa5d80eb4cdb71aef8
Ngoc Tram Nguyen <ngoctram21276667acb@gmail.com>
2026-09-13 16:48:09 +0700

    Implement CORS configuration with allowed origins and methods

--- src/server.js
+++ src/server.js
@@ -7,7 +7,37 @@ const app = express();
 
 connectDB();
 
-app.use(cors());
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://localhost:5174",
+
+  // Vercel frontend
+  "https://fe-task-management-ba3s.vercel.app",
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // Cho phép request không có Origin
+      // Ví dụ: Postman, Swagger, server-to-server
+      if (!origin) {
+        return callback(null, true);
+      }
+
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+
+      return callback(new Error("Not allowed by CORS"));
+    },
+
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+
+    allowedHeaders: ["Content-Type", "Authorization"],
+
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.get("/", (req, res) => {
   res.send("API Task Management đang hoạt động...");
